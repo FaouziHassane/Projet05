e9e8ed6c6da1ce07db47f787ef6ce0f8ff35b8c3
Hassan Faouzi <faouzi92000@gmail.com>
2022-01-19 19:30:40 +0100

    affichage du tableau récapitulatif des achat dans la page panier

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,5 @@
+let color = "";
+let qty = "";
 window.onload = function(){
     let qs=window.location.search
     let parametres = new URLSearchParams(qs)
@@ -8,13 +10,25 @@ window.onload = function(){
     .catch(error => console.error("faute"))
 }
 
+// Insertion des produits 
+
 function insertProduct (products) {
     let img = document.getElementsByClassName("item__img")
     let title = document.getElementById("title")
     let price = document.getElementById("price")
     let description = document.getElementById("description")
     let colors = document.getElementById("colors")
-
+    colors.addEventListener("change", function (e){
+        chooseColor(e, products.colors)
+    })
+    let quantity = document.getElementById("quantity")
+    quantity.addEventListener("change", function (e){
+        chooseQuantity(e)
+    })
+    let btnAddToCart = document.getElementById("addToCart")
+    btnAddToCart.addEventListener("click", function (){
+        addToCart(products._id)
+    })
     img[0].innerHTML+= '<img src="'+ products.imageUrl+'" alt="'+ products.altTxt+'">'
     title.innerHTML+= products.name
     price.innerHTML+= products.price
@@ -23,12 +37,55 @@ function insertProduct (products) {
     for (let i=0; i<products.colors.length; i++) {
         colors.innerHTML+= '<option value="'+products.colors[i]+'">'+products.colors[i]+'</option>'
     }
+}
 
+function chooseColor(e, colors) {
+    let selectedColor = colors[e.target.options.selectedIndex-1]
+    color = selectedColor;
+    console.log(color)
+}
+function chooseQuantity(e) {
+    qty = e.target.value;
+    console.log(qty)
+}
 
+//  Prépare l'ajout produits au panier
+
+function addToCart(id) {
+    let panier;
+    console.log(color);
+    console.log(qty)
+    if(localStorage.getItem("panier")) {
+        panier = JSON.parse(localStorage.getItem("panier"))
+    } else {
+        panier = []
+    }
+
+    console.log(panier)
+    let add=1
+    if(qty != "" && color != "") {
+        for (let i =0; i<panier.length; i++){
+            console.log(panier[i].quantity)
+            if (panier[i].id==id){
+                if(panier[i].color==color){
+                    add=0
+                    panier[i].quantity=parseInt(panier[i].quantity)+parseInt(qty)
+                }
+            }
+        }
+        if (add==1){
+            let object = {id:id, color:color, quantity:qty}
+            panier.push(object)
+        }
+        console.log("Produit ajouté au panier!")
+        console.log(panier)
+    } else {
+        console.error("Veuillez choisir une couleur et une quantité")
+    }
+
+    localStorage.setItem("panier", JSON.stringify(panier))
+    
 }
 
 
-function addToCart (products) {
-    addEventListener
-}
 
